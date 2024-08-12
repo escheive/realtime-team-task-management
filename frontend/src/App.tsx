a8c8c4ea867b6b97d9582cb4744833fb2f70a15a
escheive <scheiveerik@gmail.com>
2024-08-12 14:11:09 -0700

    added registerpage, loginpage as routes to use login and register forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from '~pages/Dashboard';
+import LoginPage from '~pages/auth/LoginPage';
+import RegisterPage from '~pages/auth/RegisterPage';
 
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
---
+++ frontend/src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from '~components/users/UserRegistrationForm';
+
+const RegisterPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
