4fe480cce7e9f24e775639ccd11c86765e5ddc76
escheive <scheiveerik@gmail.com>
2024-08-13 11:58:36 -0700

    reorganized login form component

--- frontend/src/components/users/UserLoginForm.tsx
+++ frontend/src/components/users/UserLoginForm.tsx
@@ -4,7 +4,6 @@ import axios from '~utils/axiosConfig';
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [token, setToken] = useState('');
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -24,8 +23,20 @@ const Login: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+      <input 
+        type="email" 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)} 
+        placeholder="Email" 
+        required 
+      />
+      <input 
+        type="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
+        placeholder="Password" 
+        required 
+      />
       <button type="submit">Login</button>
     </form>
   );
