d03035cb9946ad4ac44c319322780686a7f65f7c
escheive <scheiveerik@gmail.com>
2024-08-13 12:32:42 -0700

    built userMenu component for use in navigation system

---
+++ frontend/src/components/navigation/UserMenu.tsx
@@ -0,0 +1,28 @@
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Avatar,
+  IconButton,
+} from '@chakra-ui/react';
+import { useAuth } from '~context/AuthContext';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+
+const UserMenu = () => {
+  const { logout } = useAuth();
+
+  return (
+    <Menu>
+      <MenuButton as={IconButton} icon={<Avatar size="sm" />} variant="link">
+        <ChevronDownIcon />
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Profile</MenuItem>
+        <MenuItem onClick={logout}>Logout</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default UserMenu;
