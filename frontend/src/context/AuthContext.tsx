9c02e40b05ecb2e392744179d0fc59943b840f5c
escheive <scheiveerik@gmail.com>
2024-08-13 12:28:48 -0700

    updated AuthContextPRovider to handle type errors better

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,6 +1,12 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 
-const AuthContext = createContext(null);
+interface AuthContextType {
+  authToken: string | null;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [authToken, setAuthToken] = useState<string | null>(localStorage.getItem('authToken'));
@@ -29,4 +35,12 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  };
+
+  return context;
+}
