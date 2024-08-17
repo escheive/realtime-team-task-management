a12f2350ce3c836b48ea2d2fe95202c5ab9fbc61
escheive <scheiveerik@gmail.com>
2024-08-17 14:38:44 -0700

    added createUserPage

---
+++ frontend/src/features/users/routes/CreateUserPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+export const CreateUserPage: React.FC = () => {
+  return (
+    <Box p={4}>
+      <Text fontSize="2xl" mb={4}>Create or Edit User</Text>
+      
+    </Box>
+  );
+};
